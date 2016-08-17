bd92ecd01d2d6fa6a78f1f6bbea5f3fe769091ce
guigur <guigur13@gmail.com>
2016-08-17 02:35:51 +0200

    merge again

--- guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
+++ guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogGuigurFrontEndBundle:Default:index.html.twig');
+        $ip = $_SERVER['REMOTE_ADDR'];
+        if ($ip == "127.0.0.1" || $ip == "127.0.0.3")
+            return $this->render('BlogGuigurFrontEndBundle:Default:index.html.twig', array($ip => 'IP'));
+        else
+            return $this->render('BlogGuigurFrontEndBundle:Default:siteEnConstruction.html.twig', array($ip => 'IP'));
     }
 }
