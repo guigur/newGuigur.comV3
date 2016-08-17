6f909b66b9d4bbc8984c289fa438b26fe1dcd752
guigur <guigur13@gmail.com>
2016-08-17 02:40:57 +0200

    added ip

--- guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
+++ guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $ip = $_SERVER['REMOTE_ADDR'];
-        if ($ip == "127.0.0.1" || $ip == "127.0.0.3")
+        if ($ip == "127.0.0.1" || $ip == "78.152.130.26")
             return $this->render('BlogGuigurFrontEndBundle:Default:index.html.twig', array($ip => 'IP'));
         else
             return $this->render('BlogGuigurFrontEndBundle:Default:siteEnConstruction.html.twig', array($ip => 'IP'));
