3c2641b25151e04f008481e165ed71b7c912bd81
guigur <guigur13@gmail.com>
2016-10-17 18:55:32 +0200

    res

---
+++ guigur.com/src/BlogGuigurFrontEndBundle/Controller/CvController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BlogGuigurFrontEndBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CvController extends Controller
+{
+    public function indexAction()
+    {
+
+            return $this->render('BlogGuigurFrontEndBundle:Default:cv.html.twig');
+    }
+}
