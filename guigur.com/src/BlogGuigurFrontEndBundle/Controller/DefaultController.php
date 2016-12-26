8b6879ac9f51ee9dfa0281088a7b4092f85b4c1e
guigur <guigur13@gmail.com>
2016-12-26 01:19:29 +0100

    added project entity

--- guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
+++ guigur.com/src/BlogGuigurFrontEndBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-            return $this->render('BlogGuigurFrontEndBundle:Default:index.html.twig', array());
+
+        $projects = $this->getDoctrine()
+            ->getRepository('BlogGuigurFrontEndBundle:Project')
+            ->findAll("project");
+
+        if (!$projects)
+            throw $this->createNotFoundException('No product found for id '.$projects);
+
+        foreach ($projects as $project) {
+            echo '<pre>';
+            var_dump($projects);
+            echo '</pre>';
+        }
+        return $this->render('BlogGuigurFrontEndBundle:Default:index.html.twig', array("Projects" => $projects));
     }
 }
